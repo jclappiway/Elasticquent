21091d1d5489d30bd1927497e3115444eb1f8bb5
jean-charles lefrancois <jcl@appiway.com>
2015-12-14 15:19:21 +0100

    Rebuild Mapping isn't possible anymore check https://www.elastic.co/guide/en/elasticsearch/reference/2.1/indices-delete-mapping.html

--- src/ElasticquentTrait.php
+++ src/ElasticquentTrait.php
@@ -471,43 +471,6 @@ trait ElasticquentTrait
     }
 
     /**
-     * Delete Mapping
-     *
-     * @return array
-     */
-    public static function deleteMapping()
-    {
-        $instance = new static;
-
-        $params = $instance->getBasicEsParams();
-
-        return $instance->getElasticSearchClient()->indices()->deleteMapping($params);
-    }
-
-    /**
-     * Rebuild Mapping
-     *
-     * This will delete and then re-add
-     * the mapping for this model.
-     *
-     * @return array
-     */
-    public static function rebuildMapping()
-    {
-        $instance = new static;
-
-        // If the mapping exists, let's delete it.
-        if ($instance->mappingExists()) {
-            $instance->deleteMapping();
-        }
-
-        // Don't need ignore conflicts because if we
-        // just removed the mapping there shouldn't
-        // be any conflicts.
-        return $instance->putMapping();
-    }
-
-    /**
      * Create Index
      *
      * @param int $shards
